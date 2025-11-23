df1c6c82ebd1fc04b5decd3837410ee2432b5d95
Vedant <vedant@acagarwal.com>
2025-11-23 21:19:16 +0000

    setup init

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCQNOyAmFU4czWrXTh31GMlOPayOH_gY80",
+  authDomain: "aithos-mock.firebaseapp.com",
+  projectId: "aithos-mock",
+  storageBucket: "aithos-mock.firebasestorage.app",
+  messagingSenderId: "631693912656",
+  appId: "1:631693912656:web:e3c65ad5d66967658dff55",
+  measurementId: "G-DY1PVGXXH1",
 };
 
 // Initialize Firebase
